910f848de5ceca2a85802117f0bedb34421638e2
Solomii <kushniryksolomiya@gmail.com>
2021-11-14 14:47:57 +0200

    [+] added api test for delete

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.delete("/register/:id", async (req, res, next) => {
       _id:id
     })
 
-    res.json({
+    res.status(200).json({
       success: true,
       message: "User successfully remove!"
     })
--- test/helpers.test.js
+++ test/helpers.test.js
@@ -1,13 +1,7 @@
 process.env.NODE_ENV = 'test';
 
-const expect = require('expect');
 const request = require('supertest');
-// const { ObjectId } = require('mongodb');
 const { User } = require('../model/user');
-
-const mongoose = require("mongoose");
-
-
 const chai = require('chai');
 const  chaiHttp = require('chai-http');
 const { app } = require('../app');
@@ -16,21 +10,15 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 const user = {
-      // _id: new ObjectId(),
+      _id: null,
       first_name: "Solomiia",
       last_name: "Kush",
-      age: 23,
-      email: "ysryg@gmail.com",
+      age: 43,
+      email: "fdfg@gmail.com",
       password: "12345tgf678"
       
 }
 
-//  beforeEach((done) => {
-//   User.remove({}).then(() => {
-//     return User.insertMany( user)
-//   }).then(() => done())
-// })
-    
 describe("User", () => {
   beforeEach((done) => {
     User.remove({}, (err) => {
@@ -46,6 +34,8 @@ describe('POST /register', () => {
       .post('/register')
       .send(user)
       .end((err, res) => {
+        user._id = res.body.user._id
+        console.log("created user with ID = "+user._id);
         res.should.have.status(201);
         res.body.should.have.property('message').eql('User successfully added!');
         res.body.user.should.have.property('first_name');
@@ -59,44 +49,14 @@ describe('POST /register', () => {
 
 });
 
-describe('POST /register', () => {
-  it('should return error when user exists', (done) => {
-    // let user = {
-    //   first_name: "Solomiia",
-    //   last_name: "Kush",
-    //   age: 30,
-    //   email: "sa@gmail.com",
-    //   password: "12345678"
-      
-    // }
-    chai.request(app)
-      .post('/register')
-      .send(user)
-      .end((err, res) => {
-        res.should.have.status(409);
-        res.body.should.have.property('message').eql('User Already Exist. Please Login');
-        done();
+describe('/register/:id', () => {
+      it('it should DELETE a user given the id', (done) => {
+                chai.request(app)
+                  .delete('/register/' + user._id)
+                  .end((err, res) => {
+                   res.should.have.status(200);
+                   res.body.should.have.property('message').eql('User successfully remove!');
+                  done();
+                });
       });
-  });
-
-});
-
-
-
-
-
-  // it("should create a new user", (done) => {
-  //   let user = 'Test user'
-
-  //   request(app)
-  //     .post('/register')
-  //     .send({ User })
-  //     .expect(200)
-  //     .expect((res) => {
-  //       expect(res.body.user).toBe(user)
-      
-  //     })
-  //     .end((err, res) => {
-  //       if (err) {
-  //         return done(err)
-  //       }
+})
